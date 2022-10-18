fc286ae11f4719ba95a8ed433277eeb94213c9e8
Developer <daniel.her@onja.org>
2022-10-18 14:04:00 +0300

    Create reusable form

---
+++ src/components/form/index.tsx
@@ -0,0 +1,102 @@
+import "./index.scss";
+import { ReactComponent as GoogleIcon } from "../../assets/google-icon.svg";
+import { ReactComponent as FacebookIcon } from "../../assets/facebook-icon.svg";
+import { ReactComponent as TwitterIcon } from "../../assets/twitter-icon.svg";
+import { ReactComponent as GithubIcon } from "../../assets/github-icon.svg";
+import { ReactComponent as SecurityIcon } from "../../assets/security-icon.svg";
+import { ReactComponent as MailIcon } from "../../assets/mail-icon.svg";
+import { useLocation } from "react-router-dom";
+
+type Props = {
+  email: string;
+  password: string;
+  setEmail: React.Dispatch<string>;
+  setPassword: React.Dispatch<string>;
+  onSubmit: React.Dispatch<any>;
+  signInWithGoogle: React.Dispatch<any>;
+  footer: any;
+};
+
+const Form = ({
+  email,
+  setEmail,
+  password,
+  setPassword,
+  onSubmit,
+  signInWithGoogle,
+  footer,
+}: Props) => {
+  const location = useLocation();
+  return (
+    <div className="card">
+      <header>
+        <h1>devchallenge</h1>
+      </header>
+
+      <div className="intro-wrapper">
+        <h2 className="intro-heading">
+          {location.pathname === "/"
+            ? "  Join thousands of learners from around the world"
+            : "Login"}
+        </h2>
+        {location.pathname === "/" && (
+          <p className="intro-paragraph">
+            Master web development by making real-life projects. There are
+            multiple paths for you to choose
+          </p>
+        )}
+      </div>
+      <div className="form-wrapper">
+        <form onSubmit={onSubmit}>
+          <div className="input-wrapper email">
+            <label htmlFor="email">
+              <MailIcon />
+            </label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper password">
+            <label htmlFor="password">
+              <SecurityIcon />
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="submit-btn">
+            {location.pathname === "/" ? "Start coding now" : "Login"}
+          </button>
+        </form>
+        <p className="alternative-registration">
+          or continue with these social profile
+        </p>
+        <ul className="social-list">
+          <li className="list-item" onClick={signInWithGoogle}>
+            <GoogleIcon />
+          </li>
+          <li className="list-item">
+            <FacebookIcon />
+          </li>
+          <li className="list-item">
+            <TwitterIcon />
+          </li>
+          <li className="list-item">
+            <GithubIcon />
+          </li>
+        </ul>
+        <p className="member-link">{footer}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
