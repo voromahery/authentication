40491b3ab98731e41e8df7667026d7f7a0f77937
Developer <daniel.her@onja.org>
2022-10-18 14:03:14 +0300

    Add register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Form from "../components/form";
+import { Link } from "react-router-dom";
+import { LOGIN } from '../utils/paths';
+
+type Props = {
+  email: string;
+  password: string;
+  signInWithGoogle: React.Dispatch<any>;
+  registerWithEmailAndPassword: any;
+  setEmail: React.Dispatch<string>;
+  setPassword: React.Dispatch<string>;
+};
+
+const Register = ({
+  email,
+  password,
+  setEmail,
+  setPassword,
+  signInWithGoogle,
+  registerWithEmailAndPassword,
+}: Props) => {
+  const register = (e: any) => {
+    e.preventDefault();
+    registerWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <div className="card-wrapper">
+      <Form
+        {...{
+          email,
+          setEmail,
+          password,
+          setPassword,
+          signInWithGoogle,
+        }}
+        onSubmit={register}
+        footer={
+          <>
+            Already a member? <Link to={LOGIN}>Login</Link>
+          </>
+        }
+      />
+    </div>
+  );
+};
+
+export default Register;
